d4a99de29d7ad0a006bbbf5d7158a9f87aa801b4
Dell <Dell@DESKTOP-BPJ4AFB>
2024-01-16 13:45:11 +0530

    User Details controller added

---
+++ Cloudstine.Core.API/Controllers/UserDetailsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cloudstine.Core.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDetailsController : ControllerBase
+    {
+    }
+}
